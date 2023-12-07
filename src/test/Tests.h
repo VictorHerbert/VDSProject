391e2b0220abf1fd3161c526b234c4400ee8eddf
Victor Herbert <victorherberts@outlook.om>
2023-12-07 05:17:31 +0100

    Added some tests

--- src/test/Tests.h
+++ src/test/Tests.h
@@ -8,7 +8,7 @@
 #include "Manager.h"
 #include <vector>
 #include <gtest/gtest.h>
-
+#include <iostream>
 
 namespace ClassProject {
 
@@ -31,28 +31,24 @@ namespace ClassProject {
 
         void SetUp() override {
             testObj = Manager({
-                {.label = "0",   .data = {.low = 0, .high = 0, .topVar = 0}},                
-                {.label = "1",   .data = {.low = 1, .high = 1, .topVar = 1}},
-                {.label = "a",   .data = {.low = 0, .high = 1, .topVar = 2}},
-                {.label = "b",   .data = {.low = 0, .high = 1, .topVar = 3}},
-                {.label = "c",   .data = {.low = 0, .high = 1, .topVar = 4}},
-                {.label = "d",   .data = {.low = 0, .high = 1, .topVar = 5}},
-                {.label = "a+b", .data = {.low = 1, .high = 3, .topVar = 2}},
-                {.label = "c*d", .data = {.low = 5, .high = 0, .topVar = 4}}
+                {.label = "0",                      .data = {.low = 0, .high = 0, .topVar = 0}},                
+                {.label = "1",                      .data = {.low = 1, .high = 1, .topVar = 1}},
+                {.label = "a", .isVariable=true,    .data = {.low = 0, .high = 1, .topVar = 2}},
+                {.label = "b", .isVariable=true,    .data = {.low = 0, .high = 1, .topVar = 3}},
+                {.label = "c", .isVariable=true,    .data = {.low = 0, .high = 1, .topVar = 4}},
+                {.label = "d", .isVariable=true,    .data = {.low = 0, .high = 1, .topVar = 5}},
+                {.label = "a+b",                    .data = {.low = 1, .high = 3, .topVar = 2}},
+                {.label = "c*d",                    .data = {.low = 5, .high = 0, .topVar = 4}}
             });           
         }
     };
 
     TEST_F(BasicTest, Constructor){        
         // false node
-        ASSERT_EQ(testObj.low(0),    0);
-        ASSERT_EQ(testObj.high(0),   0);
-        ASSERT_EQ(testObj.topVar(0), 0);
+        ASSERT_EQ(testObj.nodeData(0), (NodeData{.low=0, .high=0, .topVar=0}));
 
         // true node
-        ASSERT_EQ(testObj.low(1),    1);
-        ASSERT_EQ(testObj.high(1),   1);
-        ASSERT_EQ(testObj.topVar(1), 1);
+        ASSERT_EQ(testObj.nodeData(0), (NodeData{.low=0, .high=0, .topVar=0}));
     }
 
     TEST_F(BasicTest, FalseReturn){
@@ -70,24 +66,17 @@ namespace ClassProject {
         BDD_ID id_b = testObj.createVar("b");        
 
         // node a
-        ASSERT_EQ(testObj.low(id_a),    0);
-        ASSERT_EQ(testObj.high(id_a),   1);
-        ASSERT_EQ(testObj.topVar(id_a), id_a);    
+        ASSERT_EQ(testObj.nodeData(id_a), (NodeData{.low=0, .high=1, .topVar=id_a}));
 
         // node b
-        ASSERT_EQ(testObj.low(id_b),    0);
-        ASSERT_EQ(testObj.high(id_b),   1);
-        ASSERT_EQ(testObj.topVar(id_b), id_b);    
+        ASSERT_EQ(testObj.nodeData(id_b), (NodeData{.low=0, .high=1, .topVar=id_b}));
     }
 
     TEST_F(BasicTest, CreateVarDuplicate){                
         BDD_ID id1 = testObj.createVar("a");
         BDD_ID id2 = testObj.createVar("a");
 
-        ASSERT_EQ(testObj.low(id1),    0);
-        ASSERT_EQ(testObj.high(id1),   1);
-        ASSERT_EQ(testObj.topVar(id1), id1); 
-        
+        ASSERT_EQ(testObj.nodeData(id1), (NodeData{.low=0, .high=1, .topVar=id1}));        
         ASSERT_EQ(id1, id2);                
     }
 
@@ -118,10 +107,22 @@ namespace ClassProject {
         BDD_ID id_a = 2, id_b = 3, id_c = 4;        
 
         BDD_ID id_n = testObj.ite(id_a, id_b, id_c);        
-        ASSERT_EQ(testObj.low(id_n),    id_c);
-        ASSERT_EQ(testObj.high(id_n),   id_b);
-        ASSERT_EQ(testObj.topVar(id_n), id_a); 
+        ASSERT_EQ(testObj.nodeData(id_n), (NodeData{.low=id_c, .high=id_b, .topVar=id_a}));
     }
+
+    TEST_F(FunctionsTest, CoFactorFalse){        
+        ASSERT_EQ(testObj.low(0), testObj.coFactorFalse(0)); // constant node
+        ASSERT_EQ(testObj.low(2), testObj.coFactorFalse(2)); // variable
+        ASSERT_EQ(testObj.low(5), testObj.coFactorFalse(5)); // expression
+    }
+
+    TEST_F(FunctionsTest, CoFactorTrue){
+        ASSERT_EQ(testObj.high(0), testObj.coFactorTrue(0)); // constant node
+        ASSERT_EQ(testObj.high(2), testObj.coFactorTrue(2)); // variable
+        ASSERT_EQ(testObj.high(5), testObj.coFactorTrue(5)); // expression
+    }
+
+
 }
 
 #endif
